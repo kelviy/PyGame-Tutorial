90960c80a7a384b32a56f7d2384f97057a7c8a90
kelviy <52874397+kelviy@users.noreply.github.com>
2024-07-12 18:17:05 +0200

    Implemented pause and start screen

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -10,15 +11,17 @@ game_font = pygame.font.Font('resources/font/Pixeltype.ttf', 50)
 start_time = 0
 
 def display_score():
-    current_time = pygame.time.get_ticks()
-    score_surf = game_font.render(f"Score: {round((current_time - start_time)/1000)}", False, (64, 64, 64))
+    current_time = math.floor((pygame.time.get_ticks() - start_time) /1000)
+    score_surf = game_font.render(f"Score: {current_time}", False, (64, 64, 64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
-    print(current_time)
+    return current_time
 
 def main():
 
-    game_active = True
+    game_active = False
+    global score
+    score = 0
     sky_surface = pygame.image.load('resources/graphics/Sky.png').convert_alpha()
     ground_surface = pygame.image.load('resources/graphics/ground.png').convert_alpha()
     
@@ -32,10 +35,14 @@ def main():
     player_rect = player_surf.get_rect(midbottom = (80, 300))
     player_gravity = 0
 
-    game_over_text = game_font.render("GAME OVER!", False, "White")
-    game_over_text_rect = game_over_text.get_rect(center = (400, 100))
-    continue_text = game_font.render("Press any key to restart!", False, "White")
-    continue_text_rect = continue_text.get_rect(center = (400, 160))
+    player_stand = pygame.image.load('resources/graphics/Player/player_stand.png')
+    player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+    player_stand_rect = player_stand.get_rect(center = (400, 200))
+
+    game_title_text = game_font.render("PIXEL RUNNER", False, (111,196, 169))
+    game_over_text_rect = game_title_text.get_rect(center = (400, 50))
+    continue_text = game_font.render("Press any keyboard key to restart!", False, (111,196, 169))
+    continue_text_rect = continue_text.get_rect(center = (400, 350))
 
     # main game loop
     while True:
@@ -64,7 +71,7 @@ def main():
             screen.blit(ground_surface, (0, 300))
             # pygame.draw.rect(screen, "#c0e8ec", score_rect)
             # screen.blit(score_surf, score_rect)
-            display_score()
+            score = display_score()
 
             snail_rect.x -= 4
             if snail_rect.right <= 0:
@@ -83,10 +90,17 @@ def main():
             if snail_rect.colliderect(player_rect):
                 game_active = False
         else:
-            screen.fill("Black")
+            screen.fill((94, 129, 162))
+            score_message = game_font.render(f"Your score: {score}", False, (111, 196, 169))
+            score_message_rect = score_message.get_rect(center = (400, 330))
 
-            screen.blit(game_over_text, game_over_text_rect)
-            screen.blit(continue_text, continue_text_rect)
+            screen.blit(player_stand, player_stand_rect)
+            screen.blit(game_title_text, game_over_text_rect)
+
+            if score != 0:
+                screen.blit(score_message, score_message_rect)
+            else:
+                screen.blit(continue_text, continue_text_rect)
         
         pygame.display.update()
         clock.tick(60)
